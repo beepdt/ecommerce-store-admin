3c02b6761d82130b118a8ca96f21142f64ff21cb
Deeptangshu Bhattacharjee <info.deeptangshu@gmail.com>
2025-08-12 22:55:41 +0530

    downgrade

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -4,11 +4,12 @@ import { BillboardForm } from "./components/billboard-form";
 const BillboardPage = async ({
   params,
 }: {
-  params: { billboardId: string };
+  params: Promise<{ billboardId: string }>;
 }) => {
+  const {billboardId} = await params;
   const billboard = await prismadb.billboard.findUnique({
     where: {
-      id: params.billboardId,
+      id: billboardId,
     },
   });
 
